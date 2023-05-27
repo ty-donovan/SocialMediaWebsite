77b2e2b8dd198d5d317f199fcf50c24fa0d346f6
Tyler <td20147@gmail.com>
2023-05-27 14:30:53 -0400

    added assets and basic user authentication

--- SocialMediaWebsite/server/controllers/auth.js
+++ SocialMediaWebsite/server/controllers/auth.js
@@ -1,3 +1,41 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
+
+/* REGISTER USER */
+export const register = async (req, res) => {
+    try {
+        // destructuring the request body from mongoose schema
+        const {
+            firstName,
+            lastName,
+            email,
+            password,
+            picturePath,
+            friends,
+            location,
+            occupation
+        } = req.body;
+
+        const salt = await bcrypt.genSalt();
+        const passwordHashed = await bcrypt.hash(password, salt);
+
+        const newUser = new User({
+            firstName,
+            lastName,
+            email,
+            passwordHashed,
+            picturePath,
+            friends,
+            location,
+            occupation,
+            viewedProfile: Math.floor(Math.random() * 1000),
+            impressions: Math.floor(Math.random() * 1000),
+        });
+
+        const savedUser = await newUser.save();
+        res.status(201).json(savedUser);
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+    }
+}
